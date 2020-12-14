eef405bbd97988ac36895c16f0902e8da8fca870
juune622 <juune622@naver.com>
2020-12-14 17:35:41 +0900

    12/14

---
+++ js/dom.js
@@ -0,0 +1,38 @@
+var colors = ['red',
+  'orange',
+  'green',
+  'blue',
+  'darkmagenta',
+  'deepskyblue',
+  'darkseagreen',
+  'fuchsia',
+  'navy',
+  'saddlebrown'
+]
+
+
+function onCreateBox() {
+  var cnt = Number($('input[name="cnt"]').val());
+  for (var i = 0; i < cnt; i++) {
+    color = Math.floor(Math.random() * 10);
+    $('.wrapper').append("<div class='box "+colors[color]+"'></div>")
+  }
+}
+
+function onResetBox() {
+  $('.wrapper').empty();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+$('#btCreate').click(onCreateBox);
+$('#btReset').click(onResetBox);
